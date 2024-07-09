0658e1a39f0e24a5e27cb5f07493bb3cdd0f4ea2
KaitoKid <shresawnt2011@gmail.com>
2024-07-09 23:18:06 +0530

    Added Input method

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Good day");
+string[] log = new string[100];
+int num1, num2;
+int choice = 0;
+
+void input()
+{
+
+    Console.WriteLine("Enter the numbers: ");
+    string? s1 = Console.ReadLine();
+    string? s2 = Console.ReadLine();
+    num1 = int.Parse(s1);
+    num2 = int.Parse(s2);
+    Console.WriteLine("Operation Menu: ");
+    Console.WriteLine("1. Addition ");
+    Console.WriteLine("2. Subtraction ");
+    Console.WriteLine("3. Multiplication ");
+    Console.WriteLine("4.Division ");
+    choice = int.Parse(Console.ReadLine());
+}
+
+input();
+
--- MathGame/obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ MathGame/obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539b11f74f9bf9e61d61c8d491a338154c61cafc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63bd1995ea98aa32c43adf9041be8e020696064")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
